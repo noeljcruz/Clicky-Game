982f6c8c098c07f1affe4ef8dc43e6bd50063f2c
Noel Cruz <noel.j.cruz@outlook.com>
2019-10-29 00:20:02 -0700

    Renamed json file and modified App.js file to match naming.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Nav from "./components/Nav";
-import maps from "./map.json";
+import pokemon from "./pokemon.json";
 import Card from "./components/Card";
 import "./App.css";
 
 class App extends Component {
 
   state = {
-    maps,
+    pokemon,
     title: "Pokemon Memory Game",
     score: 0,
     topScore: 0,
@@ -16,7 +16,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ maps: this.shuffle(this.state.maps) })
+    this.setState({ pokemon: this.shuffle(this.state.pokemon) })
   }
 
   clickedCard = (id) => {
@@ -26,7 +26,7 @@ class App extends Component {
       newClicked.push(id)
       let newscore = this.state.score + 1 > this.state.topScore ? this.state.score + 1 : this.state.topScore
       this.setState({
-        maps: this.shuffle(this.state.maps),
+        pokemon: this.shuffle(this.state.pokemon),
         score: this.state.score + 1,
         topScore: newscore,
         message: "Keep it up! Gotta catch 'em all!",
@@ -36,7 +36,7 @@ class App extends Component {
     }
     else {
       this.setState({
-        maps: this.shuffle(this.state.maps),
+        pokemon: this.shuffle(this.state.pokemon),
         score: 0,
         message: "You already caught that Pokemon! Try again.",
         clicked: [],
@@ -44,12 +44,12 @@ class App extends Component {
     }
   }
 
-  shuffle = maps => {
+  shuffle = pokemon => {
     // https://stackoverflow.com/a/43235780/10503606
 
-    let newMaps = maps.sort(() =>
+    let newPokemon = pokemon.sort(() =>
       Math.random() - 0.5);
-    return newMaps;
+    return newPokemon;
   }
 
   render() {
@@ -61,7 +61,7 @@ class App extends Component {
           topScore={this.state.topScore}
         />
         <div className="flags-container">
-          {this.state.maps.map(item => (
+          {this.state.pokemon.map(item => (
             <Card
               country={item.country}
               id={item.id}
